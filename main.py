670ebd0d72045bfb5b6148a057089b5b39cee74d
lukinoo <luka.bliadze.2015@mail.ru>
2022-11-24 01:54:33 +0400

    adding pyqrcode package for generate QR svg

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
+import pyqrcode
 
-print("hello, world")
+# String which represents the QR code
+s = input("Enter any word: ")
+
+# Generate QR code
+url = pyqrcode.create(s)
+
+# creating and saving the svg file
+url.svg("./assets/QR.svg", scale=7)
